d580fe694f6f6580fbcc580c646e89f782336c9f
高岡拓 <takaokataku@takaokatakunoMacBook-Pro.local>
2021-01-25 10:41:51 +0900

    user destroy one's acount

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :logged_in_user, except: [:new, :create]
+  before_action :correct_user,     only: [:edit, :update, :destroy]
 
   def index
     if params[:name].present?
@@ -100,7 +99,4 @@ class UsersController < ApplicationController
     redirect_to(root_url) unless current_user?(@user)
   end
 
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
 end
