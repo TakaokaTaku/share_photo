f142c8f942a894ef5802d2f92763628d51d19cf1
高岡拓 <takaokataku@takaokatakunoMacBook-Pro.local>
2021-01-28 07:42:55 +0900

    Add search-form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+  before_action :logged_in_user,     only: [:search]
+
   def home
     if logged_in?
       @post  = current_user.posts.build
@@ -13,5 +15,8 @@ class StaticPagesController < ApplicationController
   end
 
   def contact
+  end
+
+  def search
   end
 end
